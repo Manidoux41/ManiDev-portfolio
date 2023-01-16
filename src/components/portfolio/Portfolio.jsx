bca0d74553ea47e9dd5fa20489d3f33b6b486f40
Manidoux41 <lherissondu41@gmail.com>
2023-01-16 19:26:35 +0100

    add data for portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,27 +6,69 @@ import kasa from "../../assets/kasa.JPG"
 import pikante from "../../assets/backend-pikante.JPG"
 import "./portfolio.css";
 
+
+const data = [
+  {
+    id: 1,
+    image: booki,
+    title: 'Bookim - Frontend HTML et CSS',
+    github: 'https://github.com/Manidoux41/booki_v5.git',
+    demo: 'https://manidoux41.github.io/booki_v5/'
+  },
+  {
+    id: 2,
+    image: ohmyfood,
+    title: 'Ohmyfood - un site utilisant SASS',
+    github: 'https://github.com/Manidoux41/ohMyfood_v4.git',
+    demo: 'https://manidoux41.github.io/ohMyfood_v4/'
+  },
+  {
+    id: 3,
+    image: lapanthere,
+    title: 'La panthère - optimisation SEO dun site web',
+    github: 'https://github.com/Manidoux41/la_panthere_v3.git',
+    demo: 'https://la-panthere-v3.vercel.app/'
+  },
+  {
+    id: 4,
+    image: pikante,
+    title: 'Pikante - Backend utilisant NodeJS et Express',
+    github: 'https://github.com/Manidoux41/backend-pikante-P6.git',
+    demo: 'https://la-panthere-v3.vercel.app/'
+  },
+  {
+    id: 5,
+    image: kasa,
+    title: 'Kasa - Application ReactJS',
+    github: 'https://github.com/Manidoux41/kasa_v3.git',
+    demo: 'https://la-panthere-v3.vercel.app/'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>Mes dernières réalisations</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={booki} alt="booki.com" />
+            <img src={image} alt={title} />
           </div>
-          <h3>Booki.com</h3>
+          <h3>{title}</h3>
           <div className="portfolio__item-cta">
             <a
-              href="https://github.com/Manidoux41/booki_v5.git"
+              href={github}
               className="btn"
               target="_blank"
             >
               Github
             </a>
             <a
-              href="https://manidoux41.github.io/booki_v5/"
+              href={demo}
               className="btn btn-primary"
               target="_blank"
             >
@@ -34,95 +76,10 @@ const Portfolio = () => {
             </a>
           </div>
         </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={ohmyfood} alt="booki.com" />
-          </div>
-          <h3>OhMyFood</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Manidoux41/ohMyfood_v4.git"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://manidoux41.github.io/ohMyfood_v4/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={lapanthere} alt="La panthere" />
-          </div>
-          <h3>La Panthère</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Manidoux41/la_panthere_v3.git"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://la-panthere-v3.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={pikante} alt="booki.com" />
-          </div>
-          <h3>Pikante</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Manidoux41/backend-pikante-P6.git"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            {/* <a
-              href="https://manidoux41.github.io/booki_v5/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a> */}
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={kasa} alt="kasa" />
-          </div>
-          <h3>Kasa</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Manidoux41/kasa_v3.git"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://la-panthere-v3.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+            )
+          })
+        }
+        
       </div>
     </section>
   );
